1b0049e5082ae325c34dee34d1f27d7881d447fe
KylianCrv <kylian.crv@hotmail.com>
2022-07-05 12:23:09 +0200

    datatypes variables

---
+++ src/Datatypes.java
@@ -0,0 +1,24 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        //Chaîne de caractères
+
+        String prenom="Kylian";
+        String nom="Carvalho";
+
+        //Caractère
+        char genre='M';
+
+        //Réels
+        // Poids : 65.025
+        //Prix 45.78€
+        float poids=65.5F;
+        float tarif=8.4F;
+
+        //Réels double précision
+        double v=45.3;
+
+        //Byte : variante d'entier court
+        byte age=25;
+
+    }
+}
