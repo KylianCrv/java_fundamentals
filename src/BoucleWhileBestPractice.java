68ba6c4c67ee6ac17ffca939c860288805dde883
KylianCrv <kylian.crv@hotmail.com>
2022-07-06 15:32:36 +0200

    Boucle While best practice

--- src/BoucleWhileBestPractice.java
+++ src/BoucleWhileBestPractice.java
@@ -3,25 +3,39 @@ import java.util.Scanner;
 public class BoucleWhileBestPractice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        Fixer le nombre de caractères à 4;
-//        Demander à l'utilisateur de reesayer tant que le code PIN ne contient pas exactement 4 chiffres
 
-//        Définition du code pin
-        System.out.print("Veuillez définir un code pin : ");
-        int codePIN = input.nextInt();
-
-//        essayer de déverouiller le telephone avec un code pin
-        int codePINATester;
-
-//        Limiter le nombre de tentatives à 3 mais ne pas utiliser la boucle for
+        int codePIN;
         while (true) {
-            System.out.print("Entrez un code PIN pour déverouiller l'appareil : ");
-            codePINATester = input.nextInt();
-            if (codePINATester == codePIN) {
-                System.out.println("Appareil déverouillé.");
+            System.out.print("Veuillez définir un code PIN : ");
+            codePIN = input.nextInt();
+            if (String.valueOf(codePIN).length() == 4) {
+                System.out.println("Code PIN défini avec succès");
                 break;
             }
-            System.out.println("Code PIN erroné. Veuillez réessayer.");
+            System.out.println("Erreur : Le code PIN doit être composé de 4 chiffres");
+        }
+
+
+        int codePINATester;
+        int nombreDeTentatives = 0;
+        while (true) {
+            System.out.print("Entrer un code PIN pour déverouiller l'appareil " +
+                    ": ");
+            codePINATester = input.nextInt();//1234
+            nombreDeTentatives++;
+            if (codePINATester == codePIN) {
+                System.out.println("**************************");
+                System.out.println("** Appareil déverouillé **");
+                System.out.println("**************************");
+                break;
+            }
+            if (nombreDeTentatives == 3) {
+                System.out.printf("Votre compte est bloqué suite à %d tentatives sans " +
+                        "success.\n", nombreDeTentatives);
+                break;
+            }
+
+            System.out.println("Code PIN Erroné.Veuillez reessayer");
         }
     }
 }
