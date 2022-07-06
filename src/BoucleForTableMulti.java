b7e714f56acebc4cdf25628b66164d4d6da86122
KylianCrv <kylian.crv@hotmail.com>
2022-07-06 12:17:32 +0200

    13. Exo table multiplication avec boucle for

--- src/BoucleForTableMulti.java
+++ src/BoucleForTableMulti.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class BoucleForTableMulti {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Entrez un nombre :");
+        int nombre = input.nextInt();
+
+        System.out.print("Départ :");
+        int depart = input.nextInt();
+
+        System.out.print("Arrêt :");
+        int arret = input.nextInt();
+
+        System.out.printf("Table de multiplication de %d\n",nombre);
+        for (int compteur = depart; compteur<=arret; compteur++){
+            int produit = nombre*compteur;
+            System.out.printf("%d x %d = %d\n",nombre, compteur,produit);
+        }
+    }
 }
