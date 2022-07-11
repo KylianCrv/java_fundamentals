60b473bce98789447717d0208e1cc3a045f4d3d6
KylianCrv <kylian.crv@hotmail.com>
2022-07-11 11:05:11 +0200

    Functions with arrays

---
+++ src/FunctionsWithArrays.java
@@ -0,0 +1,15 @@
+public class FunctionsWithArrays {
+
+    static int sumArray(int[] numbersArray){
+        int somme=0;
+        for (int number : numbersArray){
+            somme=somme+number;
+        }
+        return somme;
+    }
+
+    public static void main(String[] args) {
+        int numbers[]={2,3,4,5,6,7,8};
+        System.out.println(sumArray(numbers));
+    }
+}
