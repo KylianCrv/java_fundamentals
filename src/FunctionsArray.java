c850b138ad3520d6d20453a6928f5f680e03f6d5
KylianCrv <kylian.crv@hotmail.com>
2022-07-11 11:20:56 +0200

    Somme de 2 tableaux

---
+++ src/FunctionsArray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class FunctionsArray {
+//    {1,2,3,4,5}
+//    {6,7,8,9,10}
+//    {7,9,11,13,15}
+
+    static int[] produceSumArray(int[] array1, int[] array2) {
+        int taille = array1.length;
+        int[] resultArray = new int[taille];
+        for (int i = 0; i < taille; i++) {
+            resultArray[i] = array1[i] + array2[i];
+        }
+        return resultArray;
+    }
+
+    public static void main(String[] args) {
+        int tab1[] = {1, 2, 3, 4, 5};
+        int tab2[] = {6, 7, 8, 9, 10};
+        int[] resultTab = produceSumArray(tab1, tab2);
+        System.out.println(Arrays.toString(resultTab));
+    }
+}
