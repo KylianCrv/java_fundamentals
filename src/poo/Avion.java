60c8bc01854277214d17db9715869ee3ad742aa7
KylianCrv <kylian.crv@hotmail.com>
2022-07-12 11:24:34 +0200

    28 Ajout getters et setters

--- src/poo/Avion.java
+++ src/poo/Avion.java
@@ -1,13 +1,40 @@
 package poo;
 
 public class Avion {
-    String fabricant;
-    String modele;
-    int nbSieges;
-    String couleur;
+    private String fabricant;
+    private String modele;
+    private int nbSieges;
+    private String couleur;
 
-    String nomCompletAvion(){
+//    GETTERS ou Accesseurs
+    public String getFabricant() {
+        return fabricant;
+    }
+    public String getModele() {
+        return modele;
+    }
+    public int getNbSieges() {
+        return nbSieges;
+    }
+    public String getCouleur() {
+        return couleur;
+    }
+
+//    SETTERS ou Manipulateurs
+    public void setFabricant(String nouvelleValeur){
+        fabricant=nouvelleValeur;
+    }
+    public void setModele(String nouvelleValeur){
+        modele=nouvelleValeur;
+    }
+    public void setNbSieges(int nouvelleValeur){
+        nbSieges=nouvelleValeur;
+    }
+    public void setCouleur(String nouvelleValeur){
+        couleur=nouvelleValeur;
+    }
+    String nomCompletAvion() {
 //        return fabricant+" "+modele;
-        return String.format("%s %s",fabricant, modele);
+        return String.format("%s %s", fabricant, modele);
     }
 }
