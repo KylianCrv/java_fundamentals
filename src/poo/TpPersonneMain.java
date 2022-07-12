24d4cace740f845733df7df4ac5f591be3822ce0
KylianCrv <kylian.crv@hotmail.com>
2022-07-12 13:42:16 +0200

    Correction exo User

--- src/poo/TpPersonneMain.java
+++ src/poo/TpPersonneMain.java
@@ -3,28 +3,19 @@ package poo;
 import java.util.Scanner;
 
 public class TpPersonneMain {
-    public static void main(String[] args) {
-        User personne = new User();
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Nom : ");
-        personne.setNom(input.nextLine());
-
-        System.out.print("Prénom : ");
-        personne.setPrenom(input.nextLine());
-
-        System.out.print("Nombre d'enfants : ");
-        personne.setNbEnfants(input.nextInt());
-
-        System.out.print("Situation maritale : ");
-        personne.setMarie(input.next());
-
-        System.out.println("\n");
-
-        System.out.printf("Informations sur l'utilisateur\n" +
-                "=================\n" +
-                "%s\n%d enfants à charges\n%s",personne.nomComplet(),personne.getNbEnfants(),personne.getMarie());
-
-
+        public static void main(String[] args) {
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Nom:");
+            String nom=scanner.nextLine();
+            System.out.println("Prénom:");
+            String prenom=scanner.nextLine();
+            System.out.println("Marié(e) ? True ou False:");
+            boolean isMarried=scanner.nextBoolean();
+            System.out.println("Nombre enfants:");
+            int nbrEnfant=scanner.nextInt();
+            User personne=new User(nom,prenom,nbrEnfant,isMarried);
+            personne.afficher();
+        }
     }
-}
+
--- src/poo/User.java
+++ src/poo/User.java
@@ -1,37 +1,69 @@
 package poo;
 
 public class User {
-    private String nom;
-    private String prenom;
-    private int nbEnfants;
-    private String marie;
+
+        private String nom;
+        private String prenom;
+        private int nbrEnfant;
+        private boolean isMarried;
+
+        public User(String nom, String prenom, int nbrEnfant, boolean estMarried) {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.nbrEnfant = nbrEnfant;
+            this.isMarried = estMarried;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        /**
+         * Getter et Setterdependent des exigences des besoins métiers
+         * @commentaire Formateur
+         */
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public String getPrenom() {
+            return prenom;
+        }
+
+        public void setPrenom(String prenom) {
+            this.prenom = prenom;
+        }
+
+        public int getNbrEnfant() {
+            return nbrEnfant;
+        }
+
+        public void setNbrEnfant(int nbrEnfant) {
+            this.nbrEnfant = nbrEnfant;
+        }
+
+        public boolean isMarried() {
+            return isMarried;
+        }
+
+        public void setIsMarried(boolean estMarried) {
+            this.isMarried = estMarried;
+        }
+
+        public void afficher(){
+            String married="";
+            if(isMarried)
+                married="Marié(e)";
+            else
+                married="Célibataire";
+
+            String affichage= """
+                Nom:%s Prénom:%s
+                %s 
+                %d enfants
+                """.formatted(nom,prenom,married,nbrEnfant);
+            System.out.println(affichage);
+        }
 
 
-    public int getNbEnfants() {
-        return nbEnfants;
     }
-
-    public String getMarie() {
-        return marie;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public void setNbEnfants(int nbEnfants) {
-        this.nbEnfants = nbEnfants;
-    }
-
-    public void setMarie(String marie) {
-        this.marie = marie;
-    }
-
-    public String nomComplet() {
-        return prenom + " " + nom;
-    }
-}
